a1bf25596b0182da37bd66bfab99089b02e4b2f0
shemishtamesh <shemishtamail@gmail.com>
2024-01-19 11:08:23 +0200

    adding support for other terminals

--- ollama-chat.sh
+++ ollama-chat.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 
+TERMINAL=$TERM  # only `kitty` and `alacritty` are supported
+
 choice=$(ollama list | rofi -dmenu -l 20 -p 'model')
 
 if [[ "$choice" =~ ^NAME\ {27}* ]]; then
     exit 0
 fi
 
-kitty --execute ollama run $(echo $choice | awk '{print $1}')
+case $TERMINAL in
+    'kitty')
+        kitty --execute ollama run $(echo $choice | awk '{print $1}')
+        ;;
+    'alacritty')
+        alacritty --command ollama run $(echo $choice | awk '{print $1}')
+esac
 
